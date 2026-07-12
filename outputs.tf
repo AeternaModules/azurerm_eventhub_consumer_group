d81c62f9fea14dc2714669abe015815965fcc1de
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:29 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "eventhub_consumer_groups" {
-  description = "All eventhub_consumer_group resources"
-  value       = azurerm_eventhub_consumer_group.eventhub_consumer_groups
-}
 output "eventhub_consumer_groups_eventhub_name" {
-  description = "List of eventhub_name values across all eventhub_consumer_groups"
-  value       = [for k, v in azurerm_eventhub_consumer_group.eventhub_consumer_groups : v.eventhub_name]
+  description = "Map of eventhub_name values across all eventhub_consumer_groups, keyed the same as var.eventhub_consumer_groups"
+  value       = { for k, v in azurerm_eventhub_consumer_group.eventhub_consumer_groups : k => v.eventhub_name }
 }
 output "eventhub_consumer_groups_name" {
-  description = "List of name values across all eventhub_consumer_groups"
-  value       = [for k, v in azurerm_eventhub_consumer_group.eventhub_consumer_groups : v.name]
+  description = "Map of name values across all eventhub_consumer_groups, keyed the same as var.eventhub_consumer_groups"
+  value       = { for k, v in azurerm_eventhub_consumer_group.eventhub_consumer_groups : k => v.name }
 }
 output "eventhub_consumer_groups_namespace_name" {
-  description = "List of namespace_name values across all eventhub_consumer_groups"
-  value       = [for k, v in azurerm_eventhub_consumer_group.eventhub_consumer_groups : v.namespace_name]
+  description = "Map of namespace_name values across all eventhub_consumer_groups, keyed the same as var.eventhub_consumer_groups"
+  value       = { for k, v in azurerm_eventhub_consumer_group.eventhub_consumer_groups : k => v.namespace_name }
 }
 output "eventhub_consumer_groups_resource_group_name" {
-  description = "List of resource_group_name values across all eventhub_consumer_groups"
-  value       = [for k, v in azurerm_eventhub_consumer_group.eventhub_consumer_groups : v.resource_group_name]
+  description = "Map of resource_group_name values across all eventhub_consumer_groups, keyed the same as var.eventhub_consumer_groups"
+  value       = { for k, v in azurerm_eventhub_consumer_group.eventhub_consumer_groups : k => v.resource_group_name }
 }
 output "eventhub_consumer_groups_user_metadata" {
-  description = "List of user_metadata values across all eventhub_consumer_groups"
-  value       = [for k, v in azurerm_eventhub_consumer_group.eventhub_consumer_groups : v.user_metadata]
+  description = "Map of user_metadata values across all eventhub_consumer_groups, keyed the same as var.eventhub_consumer_groups"
+  value       = { for k, v in azurerm_eventhub_consumer_group.eventhub_consumer_groups : k => v.user_metadata }
 }
 
